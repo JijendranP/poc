5062b6ecc3c3cf2320e80ae1b563530b8d155b1d
JijendranP <75211175+JijendranP@users.noreply.github.com>
2026-04-05 19:15:03 +0530

    Update deployment1.groovy

--- vars/deployment1.groovy
+++ vars/deployment1.groovy
@@ -1,13 +1,21 @@
 def call() {
-pipeline {
-    agent any
+node {
+  
+  def image
+  def mvnHome = tool 'Maven3'
 
-    stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello World'
-            }
+  
+     stage ('checkout') {
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '9ffd4ee4-3647-4a7d-a357-5e8746463282', url: 'https://bitbucket.org/ananthkannan/myawesomeangularapprepo/']]])       
         }
-    }
+    
+    
+    stage ('Build') {
+            sh 'mvn -f MyAwesomeApp/pom.xml clean install'            
+        }
+        
+    stage ('archive') {
+            archiveArtifacts '**/*.jar'
+        }
 }
 }
